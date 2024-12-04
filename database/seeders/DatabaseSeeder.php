131fb01ce4c49c307e18a1934f18350a3a33d4b7
A7pro-fuad <79939320+A7pro-fuad@users.noreply.github.com>
2024-12-04 16:07:32 +0800

    first

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,20 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
+            PostSeeder::class,
+            CategorySeeder::class,
         ]);
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        // Category::create(['name' => 'Programming']);
-        // Category::create(['name' => 'Life']);
-        // Category::create(['name' => 'Tech']);
-        // Category::create(['name' => 'Other']);
-
     }
 }
